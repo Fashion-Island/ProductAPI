42c8abb8a8b55e8ee770655f83aba4e409931d24
ted777 <an410mu@gmail.com>
2022-08-04 21:38:24 -0700

    add stress test

--- server/database/index.js
+++ server/database/index.js
@@ -2,21 +2,21 @@ require('dotenv').config();
 const { Pool, Client } = require('pg')
 // pools will use environment variables
 //for connection information
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'products',
-  password: 'haozhu123',
-  port: 5432,
-})
-
 // const pool = new Pool({
-//   user: process.env.PGUSER,
-//   host: process.env.PGHOST,
-//   database: process.env.PGDATABASE,
-//   password: process.env.PGPASSWORD,
-//   port: process.env.PGPORT,
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'products',
+//   password: 'haozhu123',
+//   port: 5432,
 // })
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+})
 pool.connect();
 
 module.exports = pool
